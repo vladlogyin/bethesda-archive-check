7fd6d780bdbd233f93e33ca8770deb2d0245a4a6
Pickysaurus <pickysaurus@googlemail.com>
2020-05-24 10:01:04 +0100

    Cleaned up a lot of WIP code

--- src/index.ts
+++ src/index.ts
@@ -58,8 +58,6 @@ function main(context: types.IExtensionContext) {
   context.requireExtension('gamebryo-plugin-management');
 
   context.once(() => {
-    // context.api.events.on('mod-enabled', (profileId: string, modId: string) => onModEnabled(profileId, modId, context.api));
-    // context.api.events.on('mod-disabled', (profileId: string, modId: string) => context.api.dismissNotification(`archive-error-${modId}`));
     // The only way to check AFTER the auto sort is by registering for the following state change. 
     context.api.onStateChange(['session', 'plugins', 'pluginInfo'], (prev, cur) => checkForErrors(context.api, cur));
   });
@@ -77,7 +75,6 @@ async function checkForErrors(api, pluginsObj) : Promise<void> {
   if (!gameData) return;
 
   // Get the plugins for the current game.
-  // const pluginsObj = util.getSafe(state, ['session', 'plugins', 'pluginInfo'], undefined);
   if (!pluginsObj || !Object.keys(pluginsObj)) return;
 
   const plugins = Object.keys(pluginsObj).map(k => pluginsObj[k]).sort((a, b) => a.loadOrder > b.loadOrder ? 1 : -1);
@@ -106,6 +103,9 @@ async function checkForErrors(api, pluginsObj) : Promise<void> {
         }
       });
     }).reduce((prev, cur) => prev.concat(cur), []);
+
+    // If there's nothing to check, we can exit here. 
+    if (!archivesToCheck.length) return;
 
     let pos = 0;
 
@@ -172,67 +172,6 @@ async function checkForErrors(api, pluginsObj) : Promise<void> {
 
 }
 
-async function onModEnabled(profileId: string, modId: string, api: types.IExtensionApi) {
-  
-  const state = api.store.getState();
-  const activeGameId = selectors.activeGameId(state);
-  const archData = archiveData.find(d => d.gameId === activeGameId);
-  if (!archData) return;
-  const validVersion = archData.version;
-  const stagingFolder = selectors.installPath(state);
-  const mods = util.getSafe(state, ['persistent', 'mods', activeGameId], undefined);
-  if (!mods) return;
-  const mod : types.IMod = mods[modId];
-  const installPath = path.join(stagingFolder, mod.installationPath);
-  return fs.readdirAsync(installPath)
-  .then((modFiles) => {
-    const archives = modFiles.filter(f => ['.bsa', '.ba2'].indexOf(path.extname(f)) !== -1);
-
-    let pos = 0;
-    const progress = (archiveName?: string) => {
-      api.store.dispatch(actions.addNotification({
-        id: `checking-archives-${mod.id}`,
-        progress: (pos * 100)/archives.length,
-        title: 'Checking archives',
-        message: archiveName,
-        type: 'activity'
-      }));
-      ++pos;
-    }
-
-    return Bluebird.map(archives, (archive : string) => {
-      const archivePath = path.join(installPath, archive);
-      progress(archive);
-      return new Promise((resolve, reject) => {
-        return streamArchiveVersion(archivePath)
-          .then((version: number) => resolve(version !== validVersion ? {name: path.basename(archive), version, validVersion} : undefined))
-          .catch((err) => resolve());
-      })
-    })
-    // After checking all the archives
-    .then((results) => {
-      const res = results.filter(r => !!r);
-      api.dismissNotification(`checking-archives-${mod.id}`);
-      if (!res.length) return;
-      api.sendNotification({
-        id: `archive-error-${mod.id}`,
-        type: 'error', 
-        group: 'archive-errors',
-        title: 'Incompatible mod archive(s)',
-        message: 'Some BSA/BA2 files are not valid for this game.',
-        actions: [
-          {
-            title: 'More',
-            action: (dismiss) => showErrorDetailsDialog(api, installPath, profileId, mod, res, archData, dismiss)
-          },
-        ],
-      });
-    })
-    .catch(() => console.error);
-  })
-  .catch(() => console.error);
-}
-
 function showMultiErrorDetailsDialog(api: types.IExtensionApi, issues, gameData) {
   const t = api.translate;
   const thisGame = gameData.gameName;
@@ -285,69 +224,6 @@ function showMultiErrorDetailsDialog(api: types.IExtensionApi, issues, gameData)
     }]
   );
 }
-
-function showErrorDetailsDialog(api: types.IExtensionApi, inst: string, profileId: string, mod: types.IMod, result, archData, dismiss) {
-  const t = api.translate;
-  api.showDialog(
-    'error', 
-    'Incompatible mod archives',
-    {bbcode: errorText(mod, result, archData, t)},
-    [
-      {
-        label: 'Disable Mod',
-        action: () => {
-          api.store.dispatch(actions.setModEnabled(profileId, mod.id, false));
-          api.events.emit('deploy-mods', () => undefined);
-          dismiss();
-        }
-      },
-      {
-        label: 'View Files',
-        action: () => util.opn(inst).catch(err => undefined)
-      },
-      {
-        label: 'Ignore',
-        action: () => dismiss()
-      }
-    ]
-  );
-}
-
-
-function errorText(mod: types.IMod, badArchives : Object[], gameData, t) : string {
-  const thisGame = gameData.gameName;
-  const attr = mod.attributes;
-  const modName = attr.customName || attr.logicalFileName || attr.name || mod.id;
-  return `${t('The {{ext}} archives included in "{{modName}}" are incompatible with {{thisGame}}. Valid archives for this game are version {{ver}}. Using incompatible archives may cause your game to crash on load.',
-    { replace: { modName, thisGame, ver: gameData.version, ext: gameData.type } })}<br/><br/>`
-  + `[h3]${t('Incompatible files')}[/h3]<br/>`
-  + '[list]'
-  + `${badArchives.map((a : {name: string, version: number, validVersion: number}) => {
-        const compGames = archiveData.filter(g => g.version === a.version).map(g => g.gameName).join('/') || t('an unknown game');
-        return `[*][b]${a.name}[/b] ${t('is intended for use in {{compGames}} (v{{ver}}).', 
-          { replace: { ver: a.version, compGames }})}`
-      }).join('\n')}`
-  + '[/list]<br/><br/>'
-  + `${t('Please ensure you have the correct version of this mod for the game you are playing. You can fix this problem yourself by disabling these mods or extracting and deleting the invalid archives.')}`
-}
-
-// async function getArchiveVersion(filePath : string) {
-//   return genHash(filePath)
-//   .then(hash => {
-//     if (cachedArchveMD5s[hash.md5sum]) return cachedArchveMD5s[hash.md5sum];
-//     return fs.readFileAsync(filePath)
-//     .then((archive) => {
-//       const versionBytes = archive.slice(4,8);
-//       const version = versionBytes.reduce((accum, entry) => accum += parseInt(entry, 10), 0);
-//       cachedArchveMD5s[hash.md5sum] = version;
-//       return version;
-//     })
-//     .catch(err => {
-//       log('error', 'Error getting BSA/BA2 version', err);
-//       return 0;
-//     })
-//   });
-// }
 
 async function streamArchiveVersion(filePath: string) : Promise<any> {
   // Open a stream to the first 9 bytes of the file.
